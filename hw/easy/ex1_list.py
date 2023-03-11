905a3bc2263061e4ef90fde23074b0252762e0a1
Violetta <murasakitta@gmail.com>
2023-03-11 10:03:31 +0300

    ex1_list done

--- hw/easy/ex1_list.py
+++ hw/easy/ex1_list.py
@@ -9,8 +9,8 @@ import random
 clear = lambda: os.system('cls')
 clear()
 
-l1 = [random.randrange(0, 99) for _ in range(50, 100)]
-l2 = [random.randrange(0, 99) for _ in range(50, 100)]
+l1 = [random.randrange(0, 9999) for _ in range(50, 100)]
+l2 = [random.randrange(0, 9999) for _ in range(50, 100)]
 print("The 1st list:\n",  l1)
 print("\nThe 2nd list:\n", l2, "\n")
 
@@ -23,4 +23,17 @@ def matches_lists(l1,l2):
     else:
         print("Matches: ", matches)
 
+
+# лучше проверять на randrange(0,90),
+# иначе вероятность совпадений минимальная
 matches_lists(l1,l2)
+
+def non_match_elements(l1, l2):
+    non_match = []
+    for i in l1:
+        if i not in l2:
+            non_match.append(i)
+    return print("\nNo match elements: ", non_match)
+
+
+non_match_elements(l1,l2)
