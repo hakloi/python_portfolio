1616febcbad6487f268120ab5bb1ed8f48167cf8
Violetta <murasakitta@gmail.com>
2023-03-20 10:45:34 +0300

    list inviting update

--- practice/list_inviting.py
+++ practice/list_inviting.py
@@ -9,6 +9,7 @@ import os
 clear = lambda: os.system('cls')
 clear()
 
+print("###############- 1 -###############")
 l1 = ["Vainona Raider", "Johny Clo", "Barbara Gordon"]
 
 def invitings(l1):
@@ -20,6 +21,7 @@ invitings(l1)
 # Изменение списка гостей: вы только что узнали, что один из гостей прийти не сможет, 
 # поэтому вам придется разослать новые приглашения . Отсутствующего гостя нужно
 # заменить кем-то другим.
+print("###############- 2 -###############")
 
 removed_guest = "Johny Clo"
 l1.remove(removed_guest)
@@ -31,4 +33,27 @@ invitings(l1)
 
 # Больше гостей: вы решили купить обеденный стол большего размера . Дополнительные 
 # места позволяют пригласить на обед еще трех гостей 
-print("Congratulations! Our dinner table allows to extend inviting's list!")
+print("###############- 3 -###############")
+print("Congratulations! Our dinner table allows to extend inviting's list!\n")
+
+l1.insert(0, "Kylie Jenner")
+l1.insert(3, "Cortny Kardashian")
+l1.append("Lupa Pupa")
+
+invitings(l1)
+
+# Сокращение списка гостей: только что выяснилось, что новый обеденный стол
+# привезти вовремя не успеют, и места хватит только для двух гостей
+print("###############- 4 -###############")
+
+lenght = len(l1)
+while (lenght > 2 ):
+    l1.pop(0)
+    lenght = lenght - 1
+print("New list of guests:\n")
+invitings(l1)
+
+del l1[1]
+del l1[0]
+
+print(l1)
