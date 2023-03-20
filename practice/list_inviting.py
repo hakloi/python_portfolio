d280de947b91a1713a2b3b586031bfe23d8bea3a
Violetta <murasakitta@gmail.com>
2023-03-20 09:33:46 +0300

    list inviting update

--- practice/list_inviting.py
+++ practice/list_inviting.py
@@ -16,3 +16,19 @@ def invitings(l1):
         print("Dear " + el +",\nI want to invite you in my party tonight!\n")
         
 invitings(l1)
+
+# Изменение списка гостей: вы только что узнали, что один из гостей прийти не сможет, 
+# поэтому вам придется разослать новые приглашения . Отсутствующего гостя нужно
+# заменить кем-то другим.
+
+removed_guest = "Johny Clo"
+l1.remove(removed_guest)
+
+print("Sorry, but guest "+ removed_guest+ " can't go to your party.\n")
+
+l1.append("Kira Love")
+invitings(l1)
+
+# Больше гостей: вы решили купить обеденный стол большего размера . Дополнительные 
+# места позволяют пригласить на обед еще трех гостей 
+print("Congratulations! Our dinner table allows to extend inviting's list!")
