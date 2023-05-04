e30e56f2f606db1ef9c6cad43498fc58d8c1a739
Violetta <murasakitta@gmail.com>
2023-05-04 11:05:57 +0300

    text2 update

--- hw/medium/text2.py
+++ hw/medium/text2.py
@@ -6,3 +6,26 @@
 # букв и пробелов. Между каждой парой слов будет ровно один пробел,
 # при этом перед первым словом и после последнего слова их не будет.
 # Максимальная длина строки — 800 символов.
+
+# cleaner
+import os
+
+clear = lambda: os.system('cls')
+clear()
+
+# code begins
+def intro (name):
+    num = int(input("Welcome, " + name + "!" +
+          "\nDo you want to try this app?" +
+          "\n   1. Yes" + "\n   2. No\n"))
+    if num == 1:
+        phrase = input("Let's game begim!\nWrite something: ")
+        print("Counter: ", len(phrase.split(" ")))
+        
+    else:
+        print("I hope you will change your mind later!")
+    
+name = input("Name: ")
+intro(name)
+
+
