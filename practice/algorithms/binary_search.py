33be98a0a1cb4b9d08ef5bd546d3ce8a4cef5320
Violetta <murasakitta@gmail.com>
2023-07-31 11:31:15 +0300

    binary search DONE

--- practice/algorithms/binary_search.py
+++ practice/algorithms/binary_search.py
@@ -1,5 +1,4 @@
 import os
-import random
 
 # code begins
 clear = lambda: os.system('cls')
@@ -8,24 +7,22 @@ clear()
 def binary_search(lst, item):
     low = 0 
     high = len(lst) - 1 
-    print(low, high)
-    
     while low <= high: 
-        mid = (low + high)//2
-        print(mid)
-        guess = lst[mid]
-        if guess == item:
-            return mid
-        if guess > item: 
-            high = mid - 1
+        mid_index = (low + high)//2
+        mid_value = lst[mid_index]
+        if mid_value == item:
+            return mid_index
+        elif mid_value > item: 
+            high = mid_index - 1
         else: 
-            low = mid + 1
-            return None
+            low = mid_index + 1
+    return None
 
 lst = list(range(1, 15))
 print("List: ", lst)  
   
 if binary_search(lst, 2) is not None:
-    print("Found at index", binary_search(lst, 2))
+    print("\nThe number 2",)
+    print("Is found at index", binary_search(lst, 2))
 else:
     print("Not found")
