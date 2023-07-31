bd1f1c5f800bfc7e047e5d03c9bbd42662d508da
Violetta <murasakitta@gmail.com>
2023-07-31 11:10:51 +0300

    binary search

--- practice/algorithms/binary_search.py
+++ practice/algorithms/binary_search.py
@@ -1,13 +1,31 @@
+import os
 import random
 
-# 1st rule: list is in SORTED order 
-def rand_list(n):
-    lst = []
-    for i in range(n):
-        lst.append(random.randint(0,90))
-    lst.sort()
-    print(lst)
+# code begins
+clear = lambda: os.system('cls')
+clear()
 
-list = rand_list(10)
-low = 0
-high = list - 1
+def binary_search(lst, item):
+    low = 0 
+    high = len(lst) - 1 
+    print(low, high)
+    
+    while low <= high: 
+        mid = (low + high)//2
+        print(mid)
+        guess = lst[mid]
+        if guess == item:
+            return mid
+        if guess > item: 
+            high = mid - 1
+        else: 
+            low = mid + 1
+            return None
+
+lst = list(range(1, 15))
+print("List: ", lst)  
+  
+if binary_search(lst, 2) is not None:
+    print("Found at index", binary_search(lst, 2))
+else:
+    print("Not found")
