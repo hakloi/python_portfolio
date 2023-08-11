0684c298b63875469876fb7adbc339667d53b977
Violetta <murasakitta@gmail.com>
2023-08-11 23:21:32 +0300

    cashing base

--- practice/algorithms/hash_table.py
+++ practice/algorithms/hash_table.py
@@ -1,2 +1,12 @@
 # constatnt time - It means the time taken will stay the same, regardless of how 
 # big the hash table is.
+
+cache = {}
+
+def get_page(url):
+    if cache.get(url):
+        return cache[url] 
+    else:
+        data = get_data_from_server(url)
+        cache[url] = data
+        return data
