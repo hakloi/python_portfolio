14d2f8ccb6df966be20b58a9ff8ab9f42ef26d17
Violetta <murasakitta@gmail.com>
2023-08-12 23:56:31 +0300

    hash table continue

---
+++ practice/algorithms/breadth_first_search.py
@@ -0,0 +1,6 @@
+from collections import deque
+import graphlib
+
+
+search_queue = deque() 
+search_queue += graphlib["you"]
--- practice/algorithms/hash_table.py
+++ practice/algorithms/hash_table.py
@@ -1,7 +1,14 @@
 # constatnt time - It means the time taken will stay the same, regardless of how 
 # big the hash table is.
 
-cache = {}
+import os
+from time import sleep
+import uuid
+
+# code begins
+clear = lambda: os.system('cls')
+clear()
+
 
 def get_page(url):
     if cache.get(url):
@@ -10,3 +17,26 @@ def get_page(url):
         data = get_data_from_server(url)
         cache[url] = data
         return data
+
+# def get_data_from_server():
+    
+
+def user_search():
+    user = input("Search Engine: ")
+    i = 0;
+    url = uuid.uuid4()
+    while (i < 3):
+        print(". . .")
+        sleep(0.5)
+        i+=1
+    print("There is your page!")
+    my_list = [user, url]
+    return {"user": user, "url": url}
+
+cache = {}
+
+my_list = user_search()
+
+cache.update(my_list)
+print(cache)
+
