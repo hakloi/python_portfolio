36ac20cf5ed4ca9609a9cfe3ff643f7f5597761d
Violetta <murasakitta@gmail.com>
2023-08-13 01:02:19 +0300

    bfs

--- practice/algorithms/breadth_first_search.py
+++ practice/algorithms/breadth_first_search.py
@@ -1,6 +1,40 @@
 from collections import deque
-import graphlib
+import os 
+
+# code begins
+clear = lambda: os.system('cls')
+clear()
 
 
-search_queue = deque() 
-search_queue += graphlib["you"]
+def person_is_seller(name): 
+    return name[-1] == 's'
+
+graph = {}
+graph["violett"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = ["miles"]
+graph["jonny"] = []
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []  
+
+    while search_queue:
+        person = search_queue.popleft()
+
+        if not person in searched: 
+            if person_is_seller(person):
+                print("It's him -", person)
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person) 
+
+    return False
+
+search('violett')
