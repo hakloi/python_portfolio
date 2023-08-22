a25570a5d10f33253480bfae2ac81c9e3209abf3
Violetta <murasakitta@gmail.com>
2023-08-22 17:02:54 +0300

    quicksort update

--- practice/algorithms/quicksort.py
+++ practice/algorithms/quicksort.py
@@ -20,7 +20,7 @@ def quicksortbest(array):
     if len(array) < 2: #BASE CASE = [], [element] 
         return array
     else:
-        mid = (0+len(array)-1)//2 
+        mid = (0+len(array)-1)//2 #without remainder
         pivot = array[mid]
         less = [i for i in array[1:] if i <= pivot] #срез массива с 1 эл, т.е. начало [2]
         greater = [i for i in array[1:] if i > pivot]
