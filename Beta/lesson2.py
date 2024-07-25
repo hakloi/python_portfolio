6574e51d2fd743b82dea410dc1261ecea3bed04c
Violetta <murasakitta@gmail.com>
2024-07-25 21:19:26 +0300

    update

--- Beta/lesson2.py
+++ Beta/lesson2.py
@@ -1,3 +1,12 @@
-# // 
-# %
-# .2f
+# if elif else 
+import os
+os.system('cls') #clear
+
+print("Введите первую и последнюю буквы русского алфавита.")
+first_letter = input()
+last_letter = input()
+if (first_letter == "а" or first_letter == "А") and (
+        last_letter == "я" or last_letter == "Я"):
+    print("Верно.")
+else:
+    print("Неверно.")
