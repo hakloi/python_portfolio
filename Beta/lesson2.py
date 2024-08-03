d0c6ea3f19e15bb1795c31f69bd63f52eb7ccb21
Violetta <murasakitta@gmail.com>
2024-08-03 07:00:23 +0300

    update

--- Beta/lesson2.py
+++ Beta/lesson2.py
@@ -2,11 +2,42 @@
 import os
 os.system('cls') #clear
 
-print("Введите первую и последнюю буквы русского алфавита.")
-first_letter = input()
-last_letter = input()
-if (first_letter == "а" or first_letter == "А") and (
-        last_letter == "я" or last_letter == "Я"):
-    print("Верно.")
+# saved_pwd = "qwerty"
+# pwd = input("Введите пароль для входа: ")
+# while pwd != saved_pwd:
+#     pwd = input("Введите пароль для входа: ")
+# print("Пароль верный.")
+
+# saved_pwd = "qwerty"
+# while input("Введите пароль для входа: ") != saved_pwd:
+#     pass
+# print("Пароль верный.")
+
+# walrus operator (python 3.8)
+# while (name := input("Введите имя: ")) != "ВЫХОД":
+#     print(f"Привет, {name}!")
+# print("Программа завершена.")
+
+# name = input("Введите имя: ")
+# while name != 'ВЫХОД':
+#     print(f"Привет, {name}!")
+#     name = input("Введите имя: ")
+# print("Программа завершена.")
+
+
+# for i in range(n)
+# for i in range(5):
+#     print(i)
+
+
+s = int(input())
+e = int(input())
+
+if e > s:
+    for el in range(e, s + 1):
+        print(el, end=" ")
+# elif 
 else:
-    print("Неверно.")
+    for el in range(s, e + 1, -1):
+        print(el, end=" ")
+    
