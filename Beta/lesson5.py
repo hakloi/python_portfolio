95a40e29a33cf8e92c1d79d5e69a955937af6743
Violetta <murasakitta@gmail.com>
2024-08-04 01:30:01 +0300

    update

--- Beta/lesson5.py
+++ Beta/lesson5.py
@@ -5,3 +5,14 @@ os.system('cls')
 # for i in range(1,6):
 #     for j in range(-2,0):
 #         print(i, j)
+
+# while input("Введи 'НЕТ': ") != "НЕТ":
+#     pass
+# else: 
+#     print("Завершен.")
+
+# while (text := input("Введи 'НЕТ':")) != 'НЕТ':
+#     if text == 'Beta':
+#         break
+# else:
+#     print("Завершен.")
---
+++ Beta/lesson6.py
@@ -0,0 +1,14 @@
+# Алгоритмы
+
+# s = str(input())
+# ans_count = 0
+
+# for i in range(len(s)):
+#     now_count = 0
+#     for j in range(len(s)):
+#         if s[i] == s[j]:
+#             now_count += 1
+#         if now_count > ans_count:
+#             ans = s[i]
+#             ans_count = now_count
+# print(ans, ans_count)
