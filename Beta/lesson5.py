5e394cd338407afd44bee4ac7e840badedd40404
Violetta <murasakitta@gmail.com>
2024-08-08 22:34:08 +0300

    update

--- Beta/lesson5.py
+++ Beta/lesson5.py
@@ -16,3 +16,18 @@ os.system('cls')
 #         break
 # else:
 #     print("Завершен.")
+
+# The multiplication table
+# n = int(input())
+
+# for i in range(n):
+#     for j in range(n):
+#         print((i + 1) * (j + 1), end=' ')
+#     print()
+
+# The multiplication table
+# n = int(input())
+
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         print(f'{j} * {i} = {i*j}')
--- Beta/lesson6.py
+++ Beta/lesson6.py
@@ -1,3 +1,33 @@
+import os
+os.system('cls')
+# Строки
+
+# t = input()
+# print(t[len(t) - 1])
+
+# t = input()
+# for el in t:
+#     print(el)
+
+# print(t[-1])
+
+# text = input()
+# for i, letter in enumerate(text):
+#     print(f"{i}. {letter}")
+
+word = '4'
+# word[1] = 'u'
+print(word.islower())
+print(word.isupper())
+print(word.isalpha())
+print(word.isalnum())
+
+# text = "Привет, мир!"
+# print(text[8:11]) #диапозон
+# print(text[:6]) # 0 - 6 по умолчанию
+# print(text[8:]) # 8 - до конца
+# print(text[:])
+
 # Алгоритмы
 
 # s = str(input())
@@ -12,3 +42,5 @@
 #             ans = s[i]
 #             ans_count = now_count
 # print(ans, ans_count)
+
+# import this
