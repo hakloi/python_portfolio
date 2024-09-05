effb090d56dec0cb4534c6480a29d2eb3ee406fa
Violetta <murasakitta@gmail.com>
2024-09-05 10:42:20 +0300

    update

---
+++ practice/mpgu/task1.py
@@ -0,0 +1,21 @@
+# word = input()
+word = 'Abrakadabra'
+print(word[2]) #1
+print(word[-2])
+print(word[:5])
+print(word[:-2]) 
+print(word[::2]) #5
+print(word[1::2]) #6
+print(word[::-1])
+print(word[::2][::-1]) #8
+print(len(word))
+
+# r
+# r
+# Abrak
+# Abrakadab
+# Arkdba
+# baaar
+# arbadakarbA
+# abdkrA
+# 11
