ef04a65f030314ea444025bb78f1d1659c4a972e
Violetta <murasakitta@gmail.com>
2024-09-30 10:14:50 +0300

    basics

---
+++ Beta/revision.py
@@ -0,0 +1,21 @@
+import os
+os.system('cls')
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# res = float((b - a) / c)
+
+# print(res)
+
+# summa = int(input())
+# last = input()
+
+
+# print(int(last, 2) + summa)
+
+item = input()
+payment = int(input())
+
+print(payment - int(item, 2))
