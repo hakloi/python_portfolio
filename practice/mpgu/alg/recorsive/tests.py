be7b9373838ea583ccc62bd875351f478c81953e
Violetta <murasakitta@gmail.com>
2024-10-21 11:11:07 +0300

    update

---
+++ practice/mpgu/alg/recorsive/tests.py
@@ -0,0 +1,19 @@
+# 1
+# nums = list(map(int, input().split()))
+
+# even = [x for x in nums if x % 2 == 0]
+# odd = [x for x in nums if x % 2 != 0]
+# print(len(even))
+# print(len(odd))
+
+# 2 dataset
+
+# import pandas as pd
+
+# data = pd.read_csv('data.csv')
+
+# print(data["A"].mean())
+# print(data["A"].median())
+# print(data["A"].max())
+# print(data["A"].min())
+# print(data["A"].std())
