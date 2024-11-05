75fe7a09c989b45999b2a5b234ffcf5606fa8f0b
Violetta <murasakitta@gmail.com>
2024-11-05 11:07:28 +0300

    upd

--- Beta/json.py
+++ Beta/json.py
@@ -69,3 +69,25 @@
 # result = sorted(list(set(result)))
 
 # print(*result, sep="\n")
+
+
+alphabeth = {
+    'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'E', 'Ж': 'Zh', 'З': 'Z',
+    'И': 'I', 'Й': 'I', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R',
+    'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'Kh', 'Ц': 'Tc', 'Ч': 'Ch', 'Ш': 'Sh', 
+    'Щ': 'Shch', 'Ы': 'Y', 'Э': 'E', 'Ю': 'Iu', 'Я': 'Ia',
+    'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e', 'ж': 'zh', 'з': 'z',
+    'и': 'i', 'й': 'i', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+    'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'tc', 'ч': 'ch', 'ш': 'sh', 
+    'щ': 'shch', 'ы': 'y', 'э': 'e', 'ю': 'iu', 'я': 'ia',
+    'ъ': '', 'ь': '', 'Ъ': '', 'Ь': '' 
+}
+    
+with open("cyrillic.txt", "r", encoding="utf-8") as filein, \
+     open("transliteration.txt", "w", encoding="utf-8") as fileout:
+    for line in filein:
+        translated = []
+        for char in line:
+            translated_char = alphabeth.get(char, char)
+            translated.append(translated_char)
+        fileout.write("".join(translated))
