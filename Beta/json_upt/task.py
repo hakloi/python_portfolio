eab28b6aa7baca47f2fe2feee6d8e5c37ca5cc59
Violetta <murasakitta@gmail.com>
2024-11-12 15:07:07 +0300

    upd

--- Beta/json_upt/task.py
+++ Beta/json_upt/task.py
@@ -1,20 +1,43 @@
-import json
-from sys import stdin
+# import json
+# from sys import stdin
 
-f1 = input()
+# f1 = input()
 
-with open(f1, "r", encoding="utf-8") as filein:
-    records = json.load(filein)
+# with open(f1, "r", encoding="utf-8") as filein:
+#     records = json.load(filein)
 
-for line in stdin:
-    a, b = line.replace(" ", "").split('==')
-    records[a] = b.strip("\n")
+# for line in stdin:
+#     a, b = line.replace(" ", "").split('==')
+#     records[a] = b.strip("\n")
 
 
-with open(f1, "w", encoding="utf-8") as fileout:
-    json.dump(records, fileout, ensure_ascii=False, indent=4)
+# with open(f1, "w", encoding="utf-8") as fileout:
+#     json.dump(records, fileout, ensure_ascii=False, indent=4)
 
 # data.json
 # one == один
 # two == два
 # three == три
+
+# def print_hello(name):
+#     return "Hello, " + name + "!"
+
+# name = "Yandex"
+# print(print_hello(name))
+
+    # dct = {k: v for k, v in items}
+    # res = []
+    
+    # for j in queries:
+    #     max_v = None
+    #     for k, v in dct.items():
+    #         if j >= k:
+    #             if max_v is None or v > max_v:
+    #                 max_v = v
+    #     res.append(max_v)
+    
+
+
+# queries = [1,2,3,4,5,6]
+# items = [[1,2],[3,2],[2,4],[5,6],[3,5]]
+# print(func(items, queries))
