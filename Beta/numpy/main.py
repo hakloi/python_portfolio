ad5095e3e19a25eb5bc2afbe7fcfbe899fef8635
Violetta <murasakitta@gmail.com>
2024-11-25 11:48:23 +0300

    requests

---
+++ Beta/numpy/main.py
@@ -0,0 +1,17 @@
+# import requests
+
+# req = requests.get("https://www.cian.ru/kupit-kvartiru-vtorichka/?deal_type=sale&engine_version=2&offer_type=flat&region=1&room1=1&room2=1&room3=1")
+
+# src = req.text
+# print(src)
+
+# import requests
+
+# server = "http://127.0.0.1:5000"
+
+# try:
+#     response = requests.get(server)
+#     response.raise_for_status()
+#     print(response.content.decode("utf-8"))
+# except requests.exceptions.RequestException:
+#     print("Сервер работает в штатном режиме")
