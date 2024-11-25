6fedeef85b3d28c94d6cb9fe4f00cb91171795a1
Violetta <murasakitta@gmail.com>
2024-11-26 01:11:14 +0300

    avito-parser

--- Beta/numpy/final_project_parsing/my_note.md
+++ Beta/numpy/final_project_parsing/my_note.md
@@ -4,9 +4,12 @@
 - сколько комнат, кв.м, этаж
 - ссылка на квартиру
 
+AVITO: url = 'https://www.avito.ru/moskva/kvartiry/prodam/1-komnatnye-ASgBAgICAkSSA8YQygiAWQ?context=H4sIAAAAAAAA_wEjANz_YToxOntzOjg6ImZyb21QYWdlIjtzOjc6ImNhdGFsb2ciO312FITcIwAAAA&f=ASgBAgECAkSSA8YQygiAWQFFxpoMHXsiZnJvbSI6MTAwMDAwMCwidG8iOjcwMDAwMDB9&s=104'
+
 2. https://qna.habr.com/q/662475 - парсинг был невозможен, выходила ошибка 403.
 То есть проходила блокировка. Сначала была попытка использовать больше headers, 
 чтобы сымитировать get-запрос, но ошибка все равно оставалась. Тогда пришлось
 прибегнуть к использование обхода Cloudfare.
 
-Парсинг свежих объявлений в количестве 50 штук.
+Парсинг свежих объявлений в количестве 50 штук. -> def get_html(url): -> for item in items[:10]:
+Я уменьшила до 10.
--- Beta/numpy/final_project_parsing/parsing_msc.py
+++ Beta/numpy/final_project_parsing/parsing_msc.py
@@ -1,5 +1,37 @@
 import cfscrape
 from selectolax.parser import HTMLParser
+import json
+import logging
+from datetime import datetime
+
+def load_config(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def generate_url(config):
+    """
+    Формирует URL для поиска на Avito.
+    """
+    base_url = f"https://www.avito.ru/{config['city']}/kvartiry/prodam/{config['rooms']}-komnatnye"
+    # params = config['other_params']
+    # price_from = config['price_range']['from']
+    # price_to = config['price_range']['to']
+    # full_url = f"{base_url}?price_from={price_from}&price_to={price_to}"
+    
+    # for key, value in params.items():
+    #     full_url += f"&{key}={value}"
+    
+    return base_url
+
+def generate_url_pool(config, room_range):
+    """
+    Генерирует пул URL на основе диапазона комнат.
+    """
+    url_pool = []
+    for rooms in room_range:
+        config['rooms'] = rooms
+        url_pool.append(generate_url(config))
+    return url_pool
 
 def get_session():
     """
@@ -39,7 +71,7 @@ def get_html(url):
         items = tree.css('div[data-marker="item"]') 
         
         index = 0
-        for item in items:
+        for item in items[:10]:
             index += 1
             # print(f"{index}. {item.css_first('h3').text()}")  
             name, square, floor = item.css_first('h3').text().split(", ")
@@ -49,8 +81,22 @@ def get_html(url):
         print(f"Error: {response.status_code}")
 
 def main():
-    url = 'https://www.avito.ru/moskva/kvartiry/prodam/1-komnatnye-ASgBAgICAkSSA8YQygiAWQ?context=H4sIAAAAAAAA_wEjANz_YToxOntzOjg6ImZyb21QYWdlIjtzOjc6ImNhdGFsb2ciO312FITcIwAAAA&f=ASgBAgECAkSSA8YQygiAWQFFxpoMHXsiZnJvbSI6MTAwMDAwMCwidG8iOjcwMDAwMDB9&s=104'
-    get_html(url) 
+    config = load_config('config.json')
+    
+    rooms_range = [1, 2, 3]
+    url_pool = generate_url_pool(config, rooms_range)
+    # url = 'https://www.avito.ru/moskva/kvartiry/prodam/1-komnatnye-ASgBAgICAkSSA8YQygiAWQ?context=H4sIAAAAAAAA_wEjANz_YToxOntzOjg6ImZyb21QYWdlIjtzOjc6ImNhdGFsb2ciO312FITcIwAAAA&f=ASgBAgECAkSSA8YQygiAWQFFxpoMHXsiZnJvbSI6MTAwMDAwMCwidG8iOjcwMDAwMDB9&s=104'
+    # 3-komnatnye
+    
+    print("Собранный пул URL:")
+    for url in url_pool:
+        print(f"URL: {url}")
+    
+    print("Демонстрация парсинга:")
+    if url_pool:
+        get_html(url_pool[0])  # Обработка первого URL из пула
+    else:
+        print("Пул URL пуст.")
     
     
 if __name__ == '__main__':
