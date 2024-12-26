81e5375a7a8c702e3c97c38980e5c911ad081c71
Violetta <murasakitta@gmail.com>
2024-12-26 14:24:49 +0300

    quicksort

--- Beta/algs/selection_sort.py
+++ Beta/algs/selection_sort.py
@@ -1,13 +1,12 @@
 def selectionSort(arr, n):
-    for j in range(1, n):
-        ins = arr[j]
-        i = j - 1
-        while (i >= 0 and ins < arr[i]):
-            arr[i + 1] = arr[i]
-            i -=1
-        arr[i + 1] = ins
+    for i in range(0, n - 1):
+        smallest = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[smallest]:
+                smallest = j
+        arr[i], arr[smallest] = arr[smallest], arr[i]
+        
     return arr
-
 
 arr = list(map(int, input().split()))
 n = len(arr)
