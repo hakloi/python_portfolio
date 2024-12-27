41c7a233208f2115ad5d6ca24d23f856a6d6f0b2
Violetta <murasakitta@gmail.com>
2024-12-28 02:48:28 +0300

    sort

--- Beta/algs/london_1.py
+++ Beta/algs/london_1.py
@@ -1,8 +1,8 @@
 def sort(arr, n):
-    for j in range(1, n - 1):
+    for j in range(1, n):
         ins = arr[j]
         i = j -1
-        while (i >= 0 and ins < arr[i]):
+        while (i >= 0 and ins > arr[i]):
             arr[i + 1] = arr[i]
             i -= 1
         arr[i + 1] = ins
---
+++ Beta/algs/london_2.py
@@ -0,0 +1,13 @@
+def x(arr, low, high):
+    elem = arr[high]
+    i = low
+    for j in range(i, high):
+        if arr[j] <= elem:
+            arr[i], arr[j] = arr[j], arr[i]
+            i = i + 1
+    arr[high], arr[i] = arr[i], arr[high]
+    return i
+    
+    
+arr = [5, 2, 7, 3, 4]
+print(x(arr, 0, len(arr)-1))
