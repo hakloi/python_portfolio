5982ee0f5d9b189b51719ed63b4d70ff631aaabb
Violetta <murasakitta@gmail.com>
2025-03-10 21:08:48 +0300

    leetcode

--- leetcode/20.py
+++ leetcode/20.py
@@ -1,23 +1,39 @@
+# def isValid(s):
+#     opening = ["{", "(", "["]
+#     closing = ["}", ")", "]"]
+#     stack = []
+
+#     for el in s:
+#         if el in opening:
+#             stack.append(el)
+#         elif el in closing and len(stack) >= 1:
+#             if opening.index(stack[-1]) == closing.index(el):
+#                 stack.pop()
+#         else:
+#             return False
+
+#     if stack == []:
+#         return True
+#     else:
+#         return False
+
+
+# s = "]"
+# print(isValid(s))
+
+# OTHER WAY -------------------------------------------------
 def isValid(s):
-    opening = ["{", "(", "["]
-    closing = ["}", ")", "]"]
+    dct = {"[": "]", "(": ")", "{": "}"}
     stack = []
 
     for el in s:
-        if el in opening:
+        if el in "{[(":
             stack.append(el)
-        elif el in closing and len(stack) >= 1:
-            if opening.index(stack[-1]) == closing.index(el):
-                stack.pop()
-        else:
+        elif len(stack) == 0 or dct[stack.pop()] != el:
             return False
 
-    if stack == []:
-        return True
-    else:
-        return False
+    return len(stack) == 0
 
 
-s = "]"
+s = "{]}"
 print(isValid(s))
-
